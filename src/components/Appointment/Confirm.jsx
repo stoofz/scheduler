1e872bf54c8031015e035f35b994af2d014aa273
Anthony Chambers <ajfchambers@gmail.com>
2023-06-16 09:11:51 -0400

    Create initial

---
+++ src/components/Appointment/Confirm.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "../Button";
+
+export default function Confirm(props) {
+
+  const onCancel = () => {
+    props.onCancel(props);
+  };
+
+  const onConfirm = () => {
+    props.onConfirm(props);
+  };
+
+  return (
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <section className="appointment__actions">
+        <Button danger onClick={onCancel}>Cancel</Button>
+        <Button danger onClick={onConfirm}>Confirm</Button>
+      </section>
+    </main>
+  );
+};
--- src/components/Appointment/Empty.jsx
+++ src/components/Appointment/Empty.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 export default function Empty(props) {
 
+ const onAdd = () => {
+    props.onAdd(props);
+  };
+
   return (
     <main className="appointment__add">
       <img
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
-        onClick={props.setDay}
+        onClick={onAdd}
       />
     </main>
   );
