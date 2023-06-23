255486b09b6fb42e2c4ed1e083a2659dcc80f3af
Anthony Chambers <ajfchambers@gmail.com>
2023-06-23 12:04:27 -0400

    Add functions

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,42 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  const onAdd = () => {
+    transition(CREATE);
+  };
+
   return (
     <article className="appointment">
     <Header time={props.time} />
+    {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+    {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+      )}
+
+    {mode === CREATE && (
+      <Form
+        interviewers={[]}
+        onCancel={() => back()}
+      />
+    )}
+
 
     {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
 
