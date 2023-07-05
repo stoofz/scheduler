be1412fe835201e3008e1c65827e5b29bfd71e5d
Anthony Chambers <ajfchambers@gmail.com>
2023-07-05 11:40:03 -0400

    Add test

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Appointment from "components/Appointment/index";
+
+// Appointment Tests 
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+});
+
+
